0d2e6e9c833567749e55c1fa034802a992ef94e7
Z <68kz8wgvgy@privaterelay.appleid.com>
2026-09-26 09:20:14 -0300

    Split areas into modules; wire enhanced End Ruin

--- src/world/areas.ts
+++ src/world/areas.ts
@@ -1,4 +1,67 @@
 /**
- * TEMPORARY STUB - will be replaced
+ * Landmark builders — orchestration layer.
  */
-export { buildEndRuin as buildEndRuinEnhanced } from "./end_ruin.ts";
+import type { World } from "./world.ts";
+import {
+  buildBreach,
+  buildCenter,
+  buildFields,
+  buildCitadel,
+  buildVoidCastles,
+  buildGraveyard,
+  buildRuins,
+  buildMazeValley,
+} from "./areas_a.ts";
+import {
+  buildVillage,
+  buildFrostPocket,
+  buildTomb,
+  buildAshenReaches,
+  buildRuinedCastle,
+  buildGlassworks,
+  buildPortalField,
+  buildPortalLobby,
+} from "./areas_b.ts";
+import { buildEndRuin } from "./end_ruin.ts";
+
+export function buildAllAreas(world: World): void {
+  buildBreach(world);
+  buildCenter(world);
+  buildFields(world);
+  buildCitadel(world);
+  buildVoidCastles(world);
+  buildGraveyard(world);
+  buildRuins(world);
+  buildMazeValley(world);
+  buildVillage(world);
+  buildFrostPocket(world);
+  buildTomb(world);
+  buildAshenReaches(world);
+  buildRuinedCastle(world);
+  buildPortalLobby(world);
+  buildGlassworks(world);
+  buildPortalField(world);
+  buildEndRuin(world);
+}
+
+export {
+  buildBreach,
+  buildCenter,
+  buildFields,
+  buildCitadel,
+  buildVoidCastles,
+  buildGraveyard,
+  buildRuins,
+  buildMazeValley,
+} from "./areas_a.ts";
+export {
+  buildVillage,
+  buildFrostPocket,
+  buildTomb,
+  buildAshenReaches,
+  buildRuinedCastle,
+  buildGlassworks,
+  buildPortalField,
+  buildPortalLobby,
+} from "./areas_b.ts";
+export { buildEndRuin } from "./end_ruin.ts";
